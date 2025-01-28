b2099f01d1cc650bf5db612fb7bdc5a3730d55c0
Proxypro2012 <kabir.tiwari@outlook.com>
2025-01-28 11:49:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,7 @@ with r2col1:
     st.image('whiteapple.png', "Apple 1")
 
 with r3col2:
-    st.button("Cross")
-    if st.button():
+    if st.button("Cross"):
       st.write("You clicked it!")
 
 with r2col3:
